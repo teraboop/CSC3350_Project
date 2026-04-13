5f028fbad2bbc578178c3b2595167b2eed1098bc
Seul An Kim <sakim5027@gmail.com>
2026-04-13 16:06:10 -0400

    sql create database order changed

--- EmployeeManagementSystem/src/SQL/ProjectDatabase_create.sql
+++ EmployeeManagementSystem/src/SQL/ProjectDatabase_create.sql
@@ -2,78 +2,6 @@
 CREATE DATABASE projectEmployeeData;
 USE projectEmployeeData;
 
-DROP TABLE IF EXISTS employees;
-CREATE TABLE employees (
-  empid INT NOT NULL PRIMARY KEY,
-  Fname VARCHAR(65) NOT NULL,
-  Lname VARCHAR(65) NOT NULL,
-  email VARCHAR(65) NOT NULL,
-  HireDate DATE,
-  Salary DECIMAL(10,2) NOT NULL,
-  SSN VARCHAR(12),
-  addressID INT NOT NULL,
-  FOREIGN KEY (addressID) REFERENCES addresses(addressID)
-);
-
-/***********************************************************************/
-
-DROP TABLE IF EXISTS payroll;
-CREATE TABLE payroll (
-  payID INT,
-  pay_date DATE,
-  earnings DECIMAL(8,2),
-  fed_tax DECIMAL(7,2),
-  fed_med DECIMAL(7,2),
-  fed_SS DECIMAL(7,2),
-  state_tax DECIMAL(7,2),
-  retire_401k DECIMAL(7,2),
-  health_care DECIMAL(7,2),
-  empid INT,
-  FOREIGN KEY (empid) REFERENCES employees(empid)
-);
-
-/***********************************************************************/ 
-
-DROP TABLE IF EXISTS employee_job_titles ;
-CREATE TABLE employee_job_titles (
-  empid INT NOT NULL,
-  job_title_id INT NOT NULL,
-  FOREIGN KEY (empid) REFERENCES employees(empid),
-  FOREIGN KEY (job_title_id) REFERENCES job_titles(job_title_id)
-);
-
-/***********************************************************************/ 
-
-DROP TABLE IF EXISTS job_titles;
-CREATE TABLE job_titles (
-  job_title_id INT PRIMARY KEY,
-  job_title VARCHAR(125) NOT NULL
-);
-
-/***********************************************************************/ 
-
-DROP TABLE IF EXISTS employee_division;
-CREATE TABLE employee_division (
-  empid int NOT NULL,
-  divID int NOT NULL,
-  FOREIGN KEY (empid) REFERENCES employees(empid),
-  FOREIGN KEY (divId) REFERENCES division(divID)
-) COMMENT='links employee to a division';
-
-/***********************************************************************/
-
-DROP TABLE IF EXISTS division;
-CREATE TABLE division (
-  divID int NOT NULL PRIMARY KEY,
-  Name varchar(100) DEFAULT NULL,
-  city varchar(50) NOT NULL,
-  addressLine1 varchar(50) NOT NULL,
-  addressLine2 varchar(50) DEFAULT NULL,
-  state varchar(50) DEFAULT NULL,
-  country varchar(50) NOT NULL,
-  postalCode varchar(15) NOT NULL
-) COMMENT='company divisions';
-
 /***********************************************************************/ 
 
 DROP TABLE IF EXISTS addresses;
@@ -106,3 +34,80 @@ CREATE TABLE states (
   stateID INT NOT NULL PRIMARY KEY,
   stateAbbr varchar(2) NOT NULL
 );
+
+/***********************************************************************/ 
+
+DROP TABLE IF EXISTS employees;
+CREATE TABLE employees (
+  empid INT NOT NULL PRIMARY KEY,
+  Fname VARCHAR(65) NOT NULL,
+  Lname VARCHAR(65) NOT NULL,
+  email VARCHAR(65) NOT NULL,
+  HireDate DATE,
+  Salary DECIMAL(10,2) NOT NULL,
+  SSN VARCHAR(12),
+  addressID INT NOT NULL,
+  FOREIGN KEY (addressID) REFERENCES addresses(addressID)
+);
+
+/***********************************************************************/
+
+DROP TABLE IF EXISTS payroll;
+CREATE TABLE payroll (
+  payID INT,
+  pay_date DATE,
+  earnings DECIMAL(8,2),
+  fed_tax DECIMAL(7,2),
+  fed_med DECIMAL(7,2),
+  fed_SS DECIMAL(7,2),
+  state_tax DECIMAL(7,2),
+  retire_401k DECIMAL(7,2),
+  health_care DECIMAL(7,2),
+  empid INT,
+  FOREIGN KEY (empid) REFERENCES employees(empid)
+);
+
+
+/***********************************************************************/ 
+
+DROP TABLE IF EXISTS job_titles;
+CREATE TABLE job_titles (
+  job_title_id INT PRIMARY KEY,
+  job_title VARCHAR(125) NOT NULL
+);
+
+/***********************************************************************/ 
+
+DROP TABLE IF EXISTS employee_job_titles ;
+CREATE TABLE employee_job_titles (
+  empid INT NOT NULL,
+  job_title_id INT NOT NULL,
+  FOREIGN KEY (empid) REFERENCES employees(empid),
+  FOREIGN KEY (job_title_id) REFERENCES job_titles(job_title_id)
+);
+
+/***********************************************************************/
+
+DROP TABLE IF EXISTS division;
+CREATE TABLE division (
+  divID int NOT NULL PRIMARY KEY,
+  Name varchar(100) DEFAULT NULL,
+  city varchar(50) NOT NULL,
+  addressLine1 varchar(50) NOT NULL,
+  addressLine2 varchar(50) DEFAULT NULL,
+  state varchar(50) DEFAULT NULL,
+  country varchar(50) NOT NULL,
+  postalCode varchar(15) NOT NULL
+) COMMENT='company divisions';
+
+/***********************************************************************/ 
+
+DROP TABLE IF EXISTS employee_division;
+CREATE TABLE employee_division (
+  empid int NOT NULL,
+  divID int NOT NULL,
+  FOREIGN KEY (empid) REFERENCES employees(empid),
+  FOREIGN KEY (divId) REFERENCES division(divID)
+) COMMENT='links employee to a division';
+
+
