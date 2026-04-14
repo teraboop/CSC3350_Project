1504e616c05d6b10e9664168bba76b0a54b1b619
teraboop <elippel5@yahoo.com>
2026-04-14 12:24:28 -0400

    I defined classification
    Changed classification type to an enumerated boolean value 0 for employee and 1 for admin. added new parameter roleID for assigning the value to the employee object.

--- EmployeeManagementSystem/src/Employee.java
+++ EmployeeManagementSystem/src/Employee.java
@@ -1,10 +1,21 @@
 public class Employee {
-    private String classify;
+    private enum Classify {
+        Employee(false), 
+        Admin(true);
+        private final boolean isAdmin;
+        Classify(boolean isAdmin) {
+            this.isAdmin = isAdmin;
+        }
+        public boolean isAdmin() {
+            return isAdmin;
+        }
+    };
+    private final Classify classify;
     private int empID;
     private String employmentDate;
     private String address;
-    public Employee(){
-        classify = getClassify();
+    public Employee(int roleID){
+        this.classify = (roleID == 1) ? Classify.Employee : Classify.Admin;
     }
 
     public int getEmpID(){
@@ -16,8 +27,8 @@ public class Employee {
     public String getAddress(){
         return address;
     }
-    public String getClassify(){
-        return classify;
+    public Boolean getClassify(){
+        return this.classify.isAdmin();
     }
     public void searchEmp(){
 
