bc61ad4b27475d172bd59e315bf8f76c1dc15a6e
Nick Camplone <camplonenicholas409@gmail.com>
2026-04-16 18:36:54 -0400

    Added 6 more add procedures
    CRUD functionality FTW

--- EmployeeManagementSystem/src/SQL/procedures.sql
+++ EmployeeManagementSystem/src/SQL/procedures.sql
@@ -3,6 +3,12 @@ drop procedure if exists AddAddress;
 drop procedure if exists AddCity;
 drop procedure if exists AddState;
 drop procedure if exists AddEmployeeFull;
+DROP PROCEDURE IF EXISTS AddCredentials;
+DROP PROCEDURE IF EXISTS AddPayroll;
+DROP PROCEDURE IF EXISTS AddJobTitle;
+DROP PROCEDURE IF EXISTS AddDivision;
+DROP PROCEDURE IF EXISTS AddEmployeeDivision;
+DROP PROCEDURE IF EXISTS AddEmployeeJobTitle;
 
 DELIMITER //
 
@@ -196,5 +202,183 @@ BEGIN
 	);
 	
 end //
+
+-- =====================================================
+-- PROCEDURE: AddCredentials
+-- Purpose: Inserts credentials into the credentials table
+-- =====================================================
+
+
+CREATE PROCEDURE AddCredentials(
+	IN v_emp_id INT,
+	IN v_username VARCHAR(65),
+	IN v_password_hash VARCHAR(255),
+	IN v_password_salt VARCHAR(255),
+	IN v_classification ENUM('Employee', 'Admin'),
+	IN v_last_login DATETIME,
+	IN v_locked_until DATETIME
+)
+
+BEGIN
+	
+	INSERT INTO credentials(
+		emp_id,
+		username,
+		password_hash,
+		password_salt,
+		classification,
+		last_login,
+		locked_until
+	)
+	
+	VALUES(
+		v_emp_id,
+		v_username,
+		v_password_hash,
+		v_password_salt,
+		v_classification,
+		NOW(),
+		v_locked_until
+	);
+	
+END //
+
+-- =====================================================
+-- PROCEDURE: AddPayroll
+-- Purpose: Inserts payroll info into the payroll table
+-- =====================================================
+
+
+CREATE PROCEDURE AddPayroll(
+	IN v_pay_date DATE,
+	IN v_earnings DECIMAL(8, 2),
+	IN v_fed_tax DECIMAL(7, 2),
+	IN v_fed_med DECIMAL(7, 2),
+	IN v_fed_ss DECIMAL(7, 2),
+	IN v_state_tax DECIMAL(7, 2),
+	IN v_retirement_401k DECIMAL(7, 2),
+	IN v_health_care DECIMAL(7, 2),
+	IN v_emp_id INT
+)
+	
+BEGIN
+		
+	INSERT INTO payroll(
+		pay_date,
+		earnings,
+		fed_tax,
+		fed_med,
+		fed_ss,
+		state_tax,
+		retirement_401k,
+		health_care,
+		emp_id
+		)
+		
+	VALUES(
+		v_pay_date,
+		v_earnings,
+		v_fed_tax,
+		v_fed_med,
+		v_fed_ss,
+		v_state_tax,
+		v_retirement_401k,
+		v_health_care,
+		v_emp_id
+	);
+		
+END //
+
+-- =====================================================
+-- PROCEDURE: AddJobTitle
+-- Purpose: Inserts a job title into the job titles table
+-- =====================================================
+
+CREATE PROCEDURE AddJobTitle(
+	IN v_job_title VARCHAR(125)
+)
+
+BEGIN
+	
+	INSERT INTO job_titles(job_title)
+	
+	VALUES(v_job_title);
+	
+END //
+
+-- =====================================================
+-- PROCEDURE: AddDivision
+-- Purpose: Inserts a division into the divisions table
+-- =====================================================
+
+CREATE PROCEDURE AddDivision(
+	IN v_name varchar(100),
+	IN v_city varchar(50),
+	IN v_address_line1 varchar(50),
+	IN v_address_line2 varchar(50),
+	IN v_state varchar(50),
+	IN v_country varchar(50),
+	IN v_postal_code varchar(15)
+)
+
+BEGIN
+	
+	INSERT INTO division(
+		name,
+		city,
+		address_line1,
+		address_line2,
+		state,
+		country,
+		postal_code
+	)
+	
+	VALUES(
+		v_name,
+		v_city,
+		v_address_line1,
+		v_address_line2,
+		v_state,
+		v_country,
+		v_postal_code
+	);
+
+END //
+	
+-- =====================================================
+-- PROCEDURE: AddEmployeeDivision
+-- Purpose: Assigns an employee to a division
+-- =====================================================
+
+CREATE PROCEDURE AddEmployeeDivision(
+	IN v_emp_id INT,
+	IN v_div_id INT
+)
+
+BEGIN
+	
+	INSERT INTO employee_division(emp_id, div_id)
+	
+	VALUES(v_emp_id, v_div_id);
+	
+END //
+
+-- =====================================================
+-- PROCEDURE: AddEmployeeJobTitle
+-- Purpose: Assigns an job title to an employee
+-- =====================================================
+
+CREATE PROCEDURE AddEmployeeJobTitle(
+	IN v_emp_id INT,
+	IN v_job_title_id INT
+)
+
+BEGIN
+	
+	INSERT INTO employee_job_titles(emp_id, job_title_id)
+	
+	VALUES(v_emp_id, v_job_title_id);
+	
+END //
 
 DELIMITER ;
