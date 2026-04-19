01888252d62246f988ed62bae64a11130db3f42f
teraboop <elippel5@yahoo.com>
2026-04-19 14:31:04 -0400

    Fully defined getters/setters, added other necessary employee aspects
    Added support for salary, phone#, email, ssn, and emergency contacts alongside getters/setters for each

--- EmployeeManagementSystem/src/Employee.java
+++ EmployeeManagementSystem/src/Employee.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 public class Employee {
     private enum Classify {
         Employee(false), 
@@ -15,37 +18,131 @@ public class Employee {
         }
 
     };
+
+    private class EmergencyContact {
+        private final String name;
+        private final String phone_number;
+
+        public EmergencyContact(String name, String phone_number) {
+            this.name = name;
+            this.phone_number = phone_number;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhoneNumber() {
+            return phone_number;
+        }
+    }
+
     private final Classify classify;
     private String first_name;
     private String last_name;
     private int emp_ID;
     private String employmentDate;
     private String address;
+    private float salary;
+    private String phone_number;
+    private String email;
+    private String ssn;
+    private ArrayList<EmergencyContact> emergencyContacts;
+
     public Employee(int roleID, int ID, String first_name, String last_name){
         this.classify = (roleID == 0) ? Classify.Employee : Classify.Admin;
         emp_ID = ID;
         this.first_name = first_name;
         this.last_name = last_name;
+        this.emergencyContacts = new ArrayList<>();
     }
 
+    //getters
     public int getEmpID(){
         return emp_ID;
     }
-
-    public void setEmpID(int emp_ID){
-        this.emp_ID = emp_ID;
+    public String getFullName(){
+        return first_name + " " + last_name;
     }
+
+    public String getFirstName(){
+        return first_name;
+    }
+
+    public String getLastName(){
+        return last_name;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public float getSalary(){
+        return salary;
+    }
+
     public String getEmploymentDate(){
         return employmentDate;
     }
+
     public String getAddress(){
         return address;
     }
+
     public Boolean getClassify(){
         return this.classify.isAdmin();
     }
-    public void searchEmp(){
 
+    public String getPhoneNumber(){
+        return phone_number;
     }
-    
+
+    public String getSSN(){
+        return ssn;
+    }
+
+    public ArrayList<EmergencyContact> getEmergencyContacts(){
+        return emergencyContacts;
+    }
+
+    //setters
+    public void setEmpID(int emp_ID){
+        this.emp_ID = emp_ID;
+    }
+
+    public void setFirstName(String first_name){
+        this.first_name = first_name;
+    }
+
+    public void setLastName(String last_name){
+        this.last_name = last_name;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setSalary(float salary){
+        this.salary = salary;
+    }
+
+    public void setEmploymentDate(String employmentDate){
+        this.employmentDate = employmentDate;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+
+    public void setPhoneNumber(String phone_number){
+        this.phone_number = phone_number;
+    }
+
+    public void setSSN(String ssn){
+        this.ssn = ssn;
+    }
+
+    public void addEmergencyContact(String name, String phone_number){
+        this.emergencyContacts.add(new EmergencyContact(name, phone_number));
+    }
 }
